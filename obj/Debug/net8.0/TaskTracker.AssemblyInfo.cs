057db7c7cbf0124cccb1b741a19701c596c5bec6
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2024-06-25 14:48:19 -0300

    Upgrades

--- obj/Debug/net8.0/TaskTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskTracker.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d0e039084d4fbe44d752017e63d988da69c33f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
